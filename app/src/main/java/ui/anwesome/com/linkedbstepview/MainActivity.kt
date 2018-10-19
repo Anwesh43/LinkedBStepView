7761df5bee6eadf99311eaac633a7a9b2a002527
Anwesh43 <anweshthecool0@gmail.com>
2018-10-19 10:33:05 +0530

    adding BStepView in Activity

--- app/src/main/java/ui/anwesome/com/linkedbstepview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/linkedbstepview/MainActivity.kt
@@ -2,11 +2,12 @@ package ui.anwesome.com.linkedbstepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.bstepview.BStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BStepView.create(this)
     }
 }
