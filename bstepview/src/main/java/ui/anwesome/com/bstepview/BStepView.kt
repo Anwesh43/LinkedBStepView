859d6eb76f4dabaa0d19abb3dd9d5fe8e8edbf67
Anwesh43 <anweshthecool0@gmail.com>
2018-10-19 09:30:45 +0530

    importing libraries and defining nodes

--- bstepview/src/main/java/ui/anwesome/com/bstepview/BStepView.kt
+++ bstepview/src/main/java/ui/anwesome/com/bstepview/BStepView.kt
@@ -3,3 +3,10 @@ package ui.anwesome.com.bstepview
 /**
  * Created by anweshmishra on 19/10/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.*
+import android.content.Context
+
+val nodes : Int = 5
